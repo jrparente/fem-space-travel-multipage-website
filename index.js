cf854de97462d99f12087cf9f01baf233ca1fe12
jrparente <jrparente@gmail.com>
2022-11-15 13:53:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const navToggle = document.querySelector(".nav-toggle");
+const iconHamburguer = document.querySelector(".icon-hamburguer");
+const iconClose = document.querySelector(".icon-close");
+const primaryNav = document.querySelector("#primary-navigation");
+const header = document.querySelector("header");
+
+function handleClick(event) {
+  event.preventDefault();
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false) &
+      iconClose.classList.add("hide") &
+      iconHamburguer.classList.remove("hide")
+    : navToggle.setAttribute("aria-expanded", true) &
+      iconClose.classList.remove("hide") &
+      iconHamburguer.classList.add("hide");
+  primaryNav.toggleAttribute("data-visible");
+}
+
+navToggle.addEventListener("click", handleClick);
