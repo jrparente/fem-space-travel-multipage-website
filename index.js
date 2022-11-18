80fd5eb9f9836d5aebc3471adadda3cb991a7789
jrparente <jrparente@gmail.com>
2022-11-18 09:29:23 +0000

    Layouts for destination page (mobile, tablet and desktop)

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const navToggle = document.querySelector(".nav-toggle");
 const iconHamburguer = document.querySelector(".icon-hamburguer");
 const iconClose = document.querySelector(".icon-close");
 const primaryNav = document.querySelector("#primary-navigation");
+const heroImg = document.querySelector("#hero-img");
+const tabs = document.querySelectorAll(".button-tabs");
+const tabContent = document.querySelectorAll(".tab");
 
-function handleClick(event) {
+navToggle.addEventListener("click", (event) => {
   event.preventDefault();
   primaryNav.hasAttribute("data-visible")
     ? navToggle.setAttribute("aria-expanded", false) &
@@ -13,6 +16,22 @@ function handleClick(event) {
       iconClose.classList.remove("hide") &
       iconHamburguer.classList.add("hide");
   primaryNav.toggleAttribute("data-visible");
+});
+
+function handleClick() {
+  //change image
+  heroImg.setAttribute("src", `./assets/destination/image-mars.png`);
+  // change tab content
 }
 
-navToggle.addEventListener("click", handleClick);
+// Remove show class from all content items
+function removeShow() {
+  tabContentItems.forEach((item) => {
+    item.classList.remove("show");
+  });
+}
+
+// Listen for tab item click
+tabs.forEach((tab) => {
+  this.addEventListener("click", handleClick);
+});
