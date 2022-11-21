2403c309f5ce0e7375e3ae57d19a1a40a61a3a6d
jrparente <jrparente@gmail.com>
2022-11-21 13:08:44 +0000

    Finished destination page
    Still need to fix the bottom border distance in the Tabs, for the Desktop screens

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const heroImg = document.querySelector("#hero-img");
 const tabs = document.querySelectorAll(".button-tabs");
 const tabContent = document.querySelectorAll(".tab");
 
+// Mobile Navigation Menu
+
 navToggle.addEventListener("click", (event) => {
   event.preventDefault();
   primaryNav.hasAttribute("data-visible")
@@ -18,20 +20,36 @@ navToggle.addEventListener("click", (event) => {
   primaryNav.toggleAttribute("data-visible");
 });
 
-function handleClick() {
+// Tabs (Destination Page)
+
+function handleClick(e) {
+  e.preventDefault();
+  removeShow();
   //change image
-  heroImg.setAttribute("src", `./assets/destination/image-mars.png`);
+  heroImg.setAttribute(
+    "src",
+    `./assets/destination/image-${this.innerHTML.toLowerCase()}.png`
+  );
   // change tab content
+  const thisTabContent = document.querySelector(
+    `#${this.innerHTML.toLowerCase()}-content`
+  );
+  thisTabContent.classList.add("show");
+  // change active tab
+  this.setAttribute("aria-selected", true);
 }
 
-// Remove show class from all content items
+// Remove show and active class from all content items
 function removeShow() {
-  tabContentItems.forEach((item) => {
+  tabContent.forEach((item) => {
     item.classList.remove("show");
+  });
+  tabs.forEach((item) => {
+    item.setAttribute("aria-selected", false);
   });
 }
 
 // Listen for tab item click
 tabs.forEach((tab) => {
-  this.addEventListener("click", handleClick);
+  tab.addEventListener("click", handleClick);
 });
