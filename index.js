95ba71a30adb61209e1702227b9e08e06296727d
jrparente <jrparente@gmail.com>
2022-11-22 11:19:10 +0000

    Finshed Crew page and started the technology page

--- index.js
+++ index.js
@@ -25,18 +25,30 @@ navToggle.addEventListener("click", (event) => {
 function handleClick(e) {
   e.preventDefault();
   removeShow();
-  //change image
-  heroImg.setAttribute(
-    "src",
-    `./assets/destination/image-${this.innerHTML.toLowerCase()}.png`
-  );
+  // // change active tab
+  this.setAttribute("aria-selected", true);
+  console.log(this.textContent.toLowerCase());
   // change tab content
   const thisTabContent = document.querySelector(
-    `#${this.innerHTML.toLowerCase()}-content`
+    `#${this.textContent.toLowerCase()}-content`
   );
+  console.log(thisTabContent);
   thisTabContent.classList.add("show");
-  // change active tab
-  this.setAttribute("aria-selected", true);
+  // //change image
+  const url = document.URL;
+  if (url.includes("destination")) {
+    heroImg.setAttribute(
+      "src",
+      `./assets/destination/image-${this.innerHTML.toLowerCase()}.webp`
+    );
+  } else if (url.includes("crew")) {
+    heroImg.setAttribute(
+      "src",
+      `./assets/crew/image-${this.textContent.toLowerCase()}.webp`
+    );
+  } else {
+    console.log("testing");
+  }
 }
 
 // Remove show and active class from all content items
